69583295dbb528798cff615eab7254323d472e57
sowbarnigavsb <31504504+sowbarnigavsb@users.noreply.github.com>
2018-02-01 14:58:33 +0530

    Create muleverodd.c

---
+++ muleverodd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,b,p;
+	scanf("%d%d",&a,&b);
+	p=a*b;
+           if(p%2==0)
+	printf("Even");
+	else
+	printf("Odd");
+	// your code goes here
+	return 0;
+}
